d72998d20da850b8bf79550863f9fbc7d14892fe
Sheik1419 <sheikgamer567@gmail.com>
2025-02-21 11:17:51 +0530

    recruit screens

---
+++ resources/views/recruit/list.blade.php
@@ -0,0 +1,67 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <div class="sidebodydiv px-5 py-3">
+        <div class="sidebodyhead">
+            <h4 class="m-0">Job Posting List</h4>
+            <a href="{{ route('recruit.add') }}"><button class="listbtn">+ Add Job Posting</button></a>
+        </div>
+
+        <div class="container-fluid mt-4 listtable">
+            <div class="filter-container row mb-3">
+                <div class="custom-search-container col-sm-12 col-md-8">
+                    <select class="headerDropdown form-select filter-option">
+                        <option value="All" selected>All</option>
+                    </select>
+                    <input type="text" id="customSearch" class="form-control filterInput" placeholder=" Search">
+                </div>
+
+                <div class="select1 col-sm-12 col-md-4 mx-auto">
+                    <div class="d-flex gap-3">
+                        <a href="" id="pdfLink"><img src="{{ asset('assets/images/printer.png') }}" id="print" alt="" height="35px"></a>
+                        <a href="" id="excelLink"><img src="{{ asset('assets/images/excel.png') }}" id="excel" alt="" height="35px"></a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="table-wrapper">
+                <table class="example table table-hover table-striped">
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Job ID</th>
+                            <th>Job Title</th>
+                            <th>Department</th>
+                            <th>Job Type</th>
+                            <th>Experienced Required</th>
+                            <th>Posting Date</th>
+                            <th>Status</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>JOB01</td>
+                            <td>1</td>
+                            <td>HR</td>
+                            <td>Full-Time</td>
+                            <td>2+</td>
+                            <td>21-10-2024</td>
+                            <td>Opened</td>
+                            <td>
+                                <div class="d-flex gap-3">
+                                    <a href="{{ route('recruit.edit') }}" data-bs-toggle="tooltip" data-bs-title="Edit">
+                                        <i class="fa-solid fa-pen-to-square"></i>
+                                    </a>
+                                </div>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+
+@endsection
