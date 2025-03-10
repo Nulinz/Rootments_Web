dd629ee9e4b57c08fcb5354b0468907e33b2be3f
Hari1196 <10harishankar11@gmail.com>
2025-03-10 09:14:03 +0530

    commit for multpile databse

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
+use Illuminate\Support\Facades\Config;
+
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -21,5 +23,25 @@ class AppServiceProvider extends ServiceProvider
     {
         //
         // require_once app_path('helper/helper.php');
+
+
+         // Get the current Git branch
+         $branch = trim(shell_exec('git rev-parse --abbrev-ref HEAD'));
+
+         // Default DB name
+         $dbName = 'rootments';
+
+         // Set the DB name based on the current branch
+         if ($branch === 'main') {
+             $dbName = 'rootments';
+         } elseif ($branch === 'error') {
+             $dbName = 'rootments_error';
+         } else {
+             // For any other branch, append the branch name to the DB name
+             $dbName = 'rootments_' . $branch;
+         }
+
+         // Dynamically update the DB_DATABASE config
+         Config::set('database.connections.mysql.database', $dbName);
     }
 }
